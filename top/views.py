270267a4ac17615fd21670a58c3f15ab16da535a
tsutomu3011 <t.08032323556@gmail.com>
2020-02-17 21:17:50 +0900

    create wallet view

--- top/views.py
+++ top/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'top/index.html')
+
+def wallet(request):
+  return render(request, 'top/wallet.html')
+
+def photo(request):
+  return render(request, 'top/photo.html')
