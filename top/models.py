05c2b18df4b7719cda0e2f010e2e44f192f90473
tsutomu3011 <t.08032323556@gmail.com>
2020-02-18 18:32:00 +0900

    pillowのインストー

--- top/models.py
+++ top/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+  situation = models.CharField(max_length=200)
+  image = models.ImageField(upload_to='images/')
