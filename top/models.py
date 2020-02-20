571ca7f0be619dd5ba6c6860ba57e480bb6a7402
tsutomu3011 <t.08032323556@gmail.com>
2020-02-20 17:34:20 +0900

    画像アップロード機能の完成

--- top/models.py
+++ top/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Photo(models.Model):
     situation = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
--- top/views.py
+++ top/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Photo
+from .forms import PhotoForm
 
 # Create your views here.
 def index(request):
-  return render(request, 'top/index.html')
+    return render(request, 'top/index.html')
 
 def wallet(request):
-  return render(request, 'top/wallet.html')
+    return render(request, 'top/wallet.html')
 
 def photo(request):
-  return render(request, 'top/photo.html')
-
+    if request.method == "POST":
+        form = PhotoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('top:photo')
+    else:
+        form = PhotoForm()
+        photos = Photo.objects.all()
+        context = {'photos':photos, 'form':form}
+        return render(request, 'top/photo.html', context)
