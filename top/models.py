176da672feded82728c21c7370b1cb3ac5cd90fb
tsutomu3011 <t.08032323556@gmail.com>
2020-02-20 11:14:38 +0900

    コンフリクトの解消

--- top/models.py
+++ top/models.py
@@ -1,11 +1,5 @@
 from django.db import models
 
-# Create your models here.
-<<<<<<< HEAD
 class Photo(models.Model):
   situation = models.CharField(max_length=200)
   image = models.ImageField(upload_to='images/')
-=======
-# class Photo(models.Model):
-
->>>>>>> 719f9dbd427a1358055276d9bbf8b0e933dd9dfd
