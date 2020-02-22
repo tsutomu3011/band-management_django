fe9bad3287ea4fcc19868983a92a8b6e126ad94c
tsutomu3011 <t.08032323556@gmail.com>
2020-02-22 14:04:32 +0900

    allowed_hostsの修正

--- bandmanagement/settings.py
+++ bandmanagement/settings.py
@@ -28,7 +28,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
