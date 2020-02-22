44a15e2ec2c70c79a6410a6ba5c601465d04ce00
tsutomu3011 <t.08032323556@gmail.com>
2020-02-22 14:21:22 +0900

    デプロイ作業

--- bandmanagement/settings.py
+++ bandmanagement/settings.py
@@ -134,8 +134,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
